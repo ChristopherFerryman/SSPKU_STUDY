7a425093acbb04dd35032cee99558baad97e08b4
Ferryman <101448408+ChristopherFerryman@users.noreply.github.com>
2022-10-16 21:48:26 +0800

    Delete package-info.java

--- package-info.java
+++
@@ -1,6 +0,0 @@
-package data_structure_homework_2;
-
-/* Stack.java是栈的接口类，Queue.java是队列的接口类
- * SeqStack.java是链式栈，实现了Stack.java接口；SeqQueue.java是链式队列，实现了Queue.java接口
- * Homework_2.java调用了SeqStack.java和SeqQueue.java的方法，并实现了对3个样例的测试
- */
